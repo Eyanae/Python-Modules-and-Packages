bc6c7d2acf4fbdf0e8d2aac23e61e717cb65d04e
ElfCoon <putinputty@yandex.com>
2024-01-31 10:43:47 +0300

    Reinitilized commit

---
+++ module_1.py
@@ -0,0 +1,17 @@
+#Task 
+"""Write a module and import all or specific objects from a module.
+Import modules using an alias name and explore the use of built-in modules.
+
+Advantages
+- Help show the importance of modules and packages.
+- Syntax and conventions for importing modules
+- Use of built-in  modules
+"""
+
+word = "Hello" 
+
+def greet(name):
+    print(word + name)
+
+class Greet:
+    pass
---
+++ task.py
@@ -0,0 +1,15 @@
+import module_1 as md
+
+"""try:
+    print(word)
+
+except NameError:
+    print("Variable 'word' does not exist!!!")"""
+
+
+md.word
+
+md.greet(" " + "Julius")
+
+md.Greet
+
